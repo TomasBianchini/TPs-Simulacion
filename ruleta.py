d025c3a08a672bf4d80f0beac9792b152e07c74a
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2024-04-06 19:25:09 -0300

    agregamos colores a las graficas

--- ruleta.py
+++ ruleta.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import sys 
 import numpy as np
 
-
+colores = ['r', 'g', 'c', 'm', 'y', 'k', 'orange', 'purple', 'brown']
 
 def GRAF_DESVIO(desvio_corridas):
     plt.figure("Valor del desvio del numero X con respecto a n")
-    plt.title("V alor del desvio del numero X con respecto a n")
+    plt.title("Valor del desvio del numero X con respecto a n")
     plt.xlabel("Numero de tiradas")
     plt.ylabel("vd (valor del desvio)")
     plt.axhline(10.67, color='b')
-    for desvio in desvio_corridas:
-        plt.plot(desvio, "-r")
+    for i, desvio in enumerate(desvio_corridas):
+        color = colores[i % len(colores)]  
+        plt.plot(desvio, color=color)
     plt.show()
 
 def GRAF_VARIANZA(varianza_corridas):
@@ -21,8 +22,9 @@ def GRAF_VARIANZA(varianza_corridas):
     plt.xlabel("Numero de tiradas")
     plt.ylabel("vv (valor de la varianza)")
     plt.axhline(114, color='b')
-    for varianza in varianza_corridas:
-        plt.plot(varianza, "-r")
+    for i, varianza in enumerate(varianza_corridas):
+        color = colores[i % len(colores)]  
+        plt.plot(varianza, color=color)
     plt.show()
 
 def GRAF_PROMEDIO(promedio_corridas):
@@ -31,8 +33,9 @@ def GRAF_PROMEDIO(promedio_corridas):
     plt.xlabel("Numero de tiradas")
     plt.ylabel("vp (valor promedio)")
     plt.axhline(18, color='b')
-    for promedio in promedio_corridas:
-        plt.plot(promedio, "-r")
+    for i, promedio in enumerate(promedio_corridas):
+        color = colores[i % len(colores)]  
+        plt.plot(promedio, color=color)
     plt.show()
 
 def GRAF_FRECUENCIA(frecuencia_relativa_corridas):
@@ -41,16 +44,15 @@ def GRAF_FRECUENCIA(frecuencia_relativa_corridas):
     plt.xlabel("Numero de tiradas")
     plt.ylabel("fr (frecuencia relativa)")
     plt.axhline(y=1/37, color='b')
-    for frecuencia_relativa in frecuencia_relativa_corridas:
-        plt.plot(frecuencia_relativa, "-r")
+    for i, frecuencia_relativa in enumerate(frecuencia_relativa_corridas):
+        color = colores[i % len(colores)]  
+        plt.plot(frecuencia_relativa, color=color)
     plt.show()
 
 
-prom = 18
 if len(sys.argv) != 7 or sys.argv[1] != "-t" or sys.argv[3] != "-c" or sys.argv[5] != "-n":
     print("Uso: python programa.py -t <num_tiradas> -c <num_corridas> -n <num>")
     sys.exit(1)
-corridas = []
 
 cant_tiradas = int(sys.argv[2])
 cant_corridas = int(sys.argv[4])
@@ -76,16 +78,7 @@ for j in range (0,cant_corridas):
         promedio_corridas.append(promedios)
         desvio_corridas.append(desvio)
         varianza_corridas.append(varianza)
-
-    for i in range(0,37):
-        frecuencia_absoluta[i] = valores.count(i)
-        # frecuencia_relativa[i] =  frecuencia_absoluta[i] / cant_tiradas
-    # print("Valores generados:", valores)
-    # for i in range(0, 37):
-        # print("Frecuencia absoluta de ", i, ":", frecuencia_absoluta[i])
-        # print("Frecuencia relativa de ", i, ":", frecuencia_relativa[i])
-    corridas.append(frecuencia_absoluta)
-
+    
 GRAF_FRECUENCIA(frecuencia_relativa_corridas)
 GRAF_PROMEDIO(promedio_corridas)
 GRAF_DESVIO(desvio_corridas)
