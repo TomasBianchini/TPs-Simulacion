3cb11ba838f5bc661c25cbadff474fd7afe89c16
Lautaro498 <lautarobrancatti03@gmail.com>
2024-04-24 13:17:14 -0300

    hicimos el metodo martingala

--- ruletaEconomia.py
+++ ruletaEconomia.py
@@ -1,1 +1,29 @@
-print('Hello world')
+import random
+
+def tirarRuleta():
+    return random.randint(0, 36) 
+
+#METODO MARTINGALA
+apuestaInicial = 5
+perdido = 0
+apostado = 'p'
+paridad = 'i'
+tirada = 1
+while paridad != apostado :
+    valorGanador = tirarRuleta()
+    if valorGanador == 0 :
+        paridad = 'n'
+    elif valorGanador % 2 == 0 :
+        paridad = 'p'
+    else: paridad = 'i'
+    #evaluo si gano
+    if paridad != apostado :
+        perdido += apuestaInicial
+        apuestaInicial *= 2
+        tirada += 1
+
+print('Ganaste en la: ', tirada)
+print('Ganaste: $', apuestaInicial - perdido)
+
+
+
