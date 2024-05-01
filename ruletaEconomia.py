ec364e28c5266cf213ae92b65fcec1868d364f38
Lautaro498 <lautarobrancatti03@gmail.com>
2024-05-01 11:01:17 -0300

    planteo completo array martingala

--- ruletaEconomia.py
+++ ruletaEconomia.py
@@ -7,14 +7,19 @@ def tirarRuleta():
 
 #METODO MARTINGALA
 n = 10
+i=0
 tiradaGanadora = []
-for i in range (1,n):
-    apuestaInicial = 5
+flujoCaja = []
+apuestaInicial = 5
+flujoCaja.append(0)
+while i <= n:
+    apuesta = apuestaInicial
     perdido = 0
     apostado = 'p' #PAR
     paridad = 'i'
     tirada = 1
     while paridad != apostado :
+        i += 1 #acumulo un giro de ruleta
         valorGanador = tirarRuleta()
         if valorGanador == 0 :
             paridad = 'n'
@@ -23,14 +28,17 @@ for i in range (1,n):
         else: paridad = 'i'
         #evaluo si gano
         if paridad != apostado :
-            perdido += apuestaInicial
-            apuestaInicial *= 2
+            perdido += apuesta
+            flujoCaja.append(-apuesta) #lo perdi. 
+            apuesta *= 2
             tirada += 1
         else: 
             tiradaGanadora.append(tirada)  #Guardamos tirada ganaroda
+            flujoCaja.append(apuesta)
 
 print('Ganaste en la: ', tirada)
-print('Ganaste: $', apuestaInicial - perdido)
+print('Ganaste: $', apuesta - perdido)
+
 
 
 
