805079c287d715735a245dd4cc439f7e1453ffd0
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2024-05-28 18:26:57 -0300

    generator  GLC

--- TP-2.1-generadores/generadores.py
+++ TP-2.1-generadores/generadores.py
@@ -1,1 +1,9 @@
-
+def generatorLGC(seed, mult, mod,inc, m) -> list:
+    # Linear congruential generator
+    numbers = [seed] # No se si es necesario agregar la semilla a la lista
+    for i in range(m):
+        next_number = ((mult*numbers[i])+inc) % mod
+        numbers.append(next_number)
+    return numbers
+h = generatorGLC(1, 1664525, 2**32 , 1013904223,10)
+print(h)
